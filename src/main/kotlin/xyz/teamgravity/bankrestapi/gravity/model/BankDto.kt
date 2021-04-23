0f76be4939806c16beb8d0addc04479891bcff3d
Raheem <raheemideadev@gmail.com>
2021-04-23 22:06:13 +0500

    docker

--- src/main/kotlin/xyz/teamgravity/bankrestapi/gravity/model/BankDto.kt
+++ src/main/kotlin/xyz/teamgravity/bankrestapi/gravity/model/BankDto.kt
@@ -1,15 +1,13 @@
 package xyz.teamgravity.bankrestapi.gravity.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class BankDto(
 
-    @JsonProperty("first_name")
+    // @JsonProperty("first_name")
     val accountNumber: String,
 
-    @JsonProperty("id")
+    // @JsonProperty("id")
     val trust: Double,
 
-    @JsonProperty("default_transaction_fee")
+    // @JsonProperty("default_transaction_fee")
     val transactionFee: Double
 )
